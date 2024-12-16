92574c587016ab5bffffc6a714d4eb7cc53fc82d
Awei <wiwi328122@gmail.com>
2024-12-16 21:54:43 +0800

    [新增]幫router註解

--- router/router.go
+++ router/router.go
@@ -16,44 +16,44 @@ func SetupRouter() *gin.Engine {
 
 	studentRouter := router.Group("/student", middleware.CheckToken())
 	{
-        studentRouter.GET("/", student.All)
-        studentRouter.GET("/:id", student.StudentInfo.Read)
-		studentRouter.PUT("/", student.StudentInfo.Update)
+        studentRouter.GET("/", student.All) //讀取所有學生
+        studentRouter.GET("/:id", student.StudentInfo.Read) //讀取一位學生
+		studentRouter.PUT("/", student.StudentInfo.Update) //更新學生資料
 	}
 
 
 	teacherRouter := router.Group("/teacher", middleware.CheckToken())
 	{
-        teacherRouter.GET("/", teacher.All)
-        teacherRouter.GET("/:id", teacher.TeacherInfo.Read)
-		teacherRouter.PUT("/", teacher.TeacherInfo.Update)
+        teacherRouter.GET("/", teacher.All)  //讀取所有教師
+        teacherRouter.GET("/:id", teacher.TeacherInfo.Read) //讀取一位教師
+		teacherRouter.PUT("/", teacher.TeacherInfo.Update) //更新教師資料
 	}
 
 	lessonRouter := router.Group("/lesson", middleware.CheckToken())
 	{
-        lessonRouter.GET("/", lesson.LessonInfo.All)
-        lessonRouter.GET("/:id", lesson.LessonInfo.Read)
-		lessonRouter.POST("/", lesson.LessonInfo.Create)
-		lessonRouter.PUT("/", lesson.LessonInfo.Update)
+        lessonRouter.GET("/", lesson.LessonInfo.All) //讀取所有課程
+        lessonRouter.GET("/:id", lesson.LessonInfo.Read) //讀取某一課程
+		lessonRouter.POST("/", lesson.LessonInfo.Create) //建立新課程
+		lessonRouter.PUT("/", lesson.LessonInfo.Update)  //更新課程資料
 	}
 
 	applyRouter := router.Group("/apply", middleware.CheckToken())
 	{
-		applyRouter.POST("/", applyList.ApplyList.Create)
-		applyRouter.PUT("/", applyList.ApplyList.Update)
-		applyRouter.GET("/searchMember", applyList.ApplyList.SearchMember)
-		applyRouter.GET("/searchLesson", applyList.ApplyList.SearchLesson)
+		applyRouter.POST("/", applyList.ApplyList.Create) //報名
+		applyRouter.PUT("/", applyList.ApplyList.Update)  //付款
+		applyRouter.GET("/searchMember", applyList.ApplyList.SearchMember) //查詢某會員參加的課程列表
+		applyRouter.GET("/searchLesson", applyList.ApplyList.SearchLesson) // 查詢某課程的參加者
 
 		applyRouter.GET("/", applyList.ApplyList.Read)
 	}
 
 
-    router.POST("/teacherRegister", teacher.TeacherInfo.Create)
-    router.POST("/register", student.StudentInfo.Create)
+    router.POST("/teacherRegister", teacher.TeacherInfo.Create) //註冊教師
+    router.POST("/register", student.StudentInfo.Create) //註冊學生
 //     router.GET("/login", login.LoginInfo.UnSignIn)
 
-    router.POST("/teacherLogin", login.LoginInfo.TeacherSignIn)
-    router.POST("/login", login.LoginInfo.SignIn)
+    router.POST("/teacherLogin", login.LoginInfo.TeacherSignIn) //老師登入
+    router.POST("/login", login.LoginInfo.SignIn)  //學生登入
 
     return router
 }
